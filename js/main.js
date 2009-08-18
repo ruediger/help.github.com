89c8115fd43a8c7a6683d4704be6472e34aab067
tekkub <tekkub@gmail.com>
2009-08-17 23:36:59 -0600

    Tweak sidebar js so that triangles are clickable

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@ $(function(){
     $(".sidebar dl#" + main_category).slideDown("slow").parent().addClass("expanded")
   }
 
-  $(".sidebar h3").click(function(){
-    if ($(this).siblings("dl").is(":hidden")) {
+  $(".sidebar li").click(function(){
+    if ($(this).children("dl").is(":hidden")) {
       $(".sidebar dl:visible").slideUp("fast").parent().removeClass("expanded")
-      $(this).siblings("dl").slideDown("fast").parent().addClass("expanded")
+      $(this).addClass("expanded").children("dl").slideDown("fast")
     }
   })
 })
