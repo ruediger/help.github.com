9f237f1ff8326de74e4640c460f095cbff6aa7f3
tekkub <tekkub@gmail.com>
2009-10-16 17:25:41 -0600

    Use jQuery UI's icon classes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 $(function(){
-  $(".sidebar").accordion({ header: 'h3', autoHeight: false, navigation: true, navigationFilter: function() {if (typeof main_category != "undefined") {return this.id == main_category}} });
+  $(".sidebar").accordion({
+    header: 'h3', autoHeight: false,
+    icons: {'header': 'ui-icon-triangle-expand', 'headerSelected': 'ui-icon-triangle-collapse'},
+    navigation: true, navigationFilter: function() {if (typeof main_category != "undefined") {return this.id == main_category}} });
 })
