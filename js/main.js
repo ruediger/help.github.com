5ef88809cc6af138409adcd1878009a36714dc67
tekkub <tekkub@gmail.com>
2009-10-16 16:57:10 -0600

    Rework sidebar to use jQuery UI

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
 $(function(){
-  if (typeof main_category == "undefined") {
-    $(".sidebar dl:first").slideDown("slow").parent().addClass("expanded")
-  } else {
-    $(".sidebar dl#" + main_category).slideDown("slow").parent().addClass("expanded")
-  }
-
-  $(".sidebar li").click(function(){
-    if ($(this).children("dl").is(":hidden")) {
-      $(".sidebar dl:visible").slideUp("fast").parent().removeClass("expanded")
-      $(this).addClass("expanded").children("dl").slideDown("fast")
-    }
-  })
+  $(".sidebar").accordion({ header: 'h3', autoHeight: false, navigation: true, navigationFilter: function() {if (typeof main_category != "undefined") {return this.id == main_category}} });
 })
